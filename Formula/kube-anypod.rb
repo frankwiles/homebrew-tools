d4ff6c3d5b55bd2425e6876d5dbac6adffb4750c
frankwiles <144159+frankwiles@users.noreply.github.com>
2026-01-29 14:59:16 +0000

    [create-pull-request] automated change

--- Formula/kube-anypod.rb
+++ Formula/kube-anypod.rb
@@ -4,13 +4,13 @@
 class KubeAnypod < Formula
   desc "Return the name of any pod from a given Deployment, StatefulSet, or DaemonSet"
   homepage "https://github.com/frankwiles/kube-anypod"
-  version "0.2.0"
+  version "0.3.0"
   license "BSD-3-Clause"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/frankwiles/kube-anypod/releases/download/v0.2.0/anypod-macos-x86_64"
-    sha256 "f79ec3a5dcbe29d55dffde8d16ccae2ebe8b2181a61c6b970ef10f43b8bdb5c0"
+    url "https://github.com/frankwiles/kube-anypod/releases/download/v0.3.0/anypod-macos-x86_64"
+    sha256 "bd27d0a281d686bfba49c367b654380618fa6c26b8cab7ea631eb7f534971c22"
 
     def install
       bin.install "anypod-macos-x86_64" => "anypod"
@@ -18,8 +18,8 @@ class KubeAnypod < Formula
   end
 
   if Hardware::CPU.arm?
-    url "https://github.com/frankwiles/kube-anypod/releases/download/v0.2.0/anypod-macos-aarch64"
-    sha256 "8405432faa59ec42bd9a924dc77a4e0d2b719a229b911697aeca89dbf16d8c12"
+    url "https://github.com/frankwiles/kube-anypod/releases/download/v0.3.0/anypod-macos-aarch64"
+    sha256 "c640bf0bf2845ebd38093eaf28b76dcc1679fe99a0a43a94c07585c0a7f872a9"
 
     def install
       bin.install "anypod-macos-aarch64" => "anypod"
