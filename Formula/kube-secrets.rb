30549d19dce5d3f672b88b2c7da0d7f768b752bb
frankwiles <144159+frankwiles@users.noreply.github.com>
2026-05-29 10:16:23 +0000

    [create-pull-request] automated change

--- Formula/kube-secrets.rb
+++ Formula/kube-secrets.rb
@@ -4,13 +4,13 @@
 class KubeSecrets < Formula
   desc "Rust command line utility to quickly display useful secrets in a Kubernetes namespace"
   homepage "https://github.com/frankwiles/kube-secrets"
-  version "0.7.1"
+  version "null"
   license "BSD-3-Clause"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/frankwiles/kube-secrets/releases/download/v0.7.1/secrets-macos-x86_64"
-    sha256 "8448578b7573aab808c39b136cfef4ff20f09aa3f9f73a9e998e8dd9602138dc"
+    url "https://github.com/frankwiles/kube-secrets/releases/download/null/secrets-macos-x86_64"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
     def install
       bin.install "secrets-macos-x86_64" => "secrets"
@@ -18,8 +18,8 @@ class KubeSecrets < Formula
   end
 
   if Hardware::CPU.arm?
-    url "https://github.com/frankwiles/kube-secrets/releases/download/v0.7.1/secrets-macos-aarch64"
-    sha256 "a4f986b9918aa77872a02ac038f5761efa064ad81f5d319be741974e1222dbbc"
+    url "https://github.com/frankwiles/kube-secrets/releases/download/null/secrets-macos-aarch64"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
     def install
       bin.install "secrets-macos-aarch64" => "secrets"
